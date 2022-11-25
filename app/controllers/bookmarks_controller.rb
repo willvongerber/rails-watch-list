e36ea889a6cd24526e2db94d41559baf822057cf
Wilhelm von Gerber <willvongerber@gmail.com>
2022-11-25 18:01:00 +0100

    rails watch list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,34 @@
 class BookmarksController < ApplicationController
+  before_action :find_list, only: [ :new, :create ]
 
-  def index
-    @bookmarks = Bookmark.all
+def new
+  @bookmark = Bookmark.new
+end
+
+def create
+  @bookmark = Bookmark.new(bookmark_params)
+  @bookmark.list = @list
+  if @bookmark.save
+    redirect_to list_path(@list)
+  else
+    render template: "lists/show"
   end
+end
 
-  def new
-    @bookmark = Bookmark.new
-  end
+def destroy
+  @bookmark = Bookmark.find(params[:id])
+  @bookmark.destroy
+  redirect_to root_path
+end
 
-  def create
-    @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
-    if @bookmark.save
-      redirect_to list_path(@list)
-    else
-      render :new
-    end
-  end
+private
 
-  def destroy
-    @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
-  end
+def find_list
+  @list = List.find(params[:list_id])
+end
 
-  private
+def bookmark_params
+  params.require(:bookmark).permit(:comment, :movie_id)
+end
 
-  def bookmark_params
-    params.require(:bookmark).permit(:comment)
-  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,30 +1,37 @@
 class ListsController < ApplicationController
+
   def index
     @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
     @list = List.new
   end
 
-  def show
-    @list = List.find(params[id])
-  end
-
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to lists_path
+    if @list.save
+    redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name, :photo)
+    params.require(:list).permit(:name, :imageUrl)
   end
+
 end
